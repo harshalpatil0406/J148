7780907c936a743c9f45981d4d999611d54ba468
harshalpatil0406 <140956375+harshalpatil0406@users.noreply.github.com>
2025-03-16 12:51:33 +0530

    Update and rename assignment_15.java to assignment_z15.java

--- Assignments/assignment_z15.java
+++ Assignments/assignment_z15.java
@@ -3,7 +3,7 @@ package Assignments;
 import java.util.Scanner;
 
 //Find reminder without using ( % ) operator
-public class assignment_15 {
+public class assignment_z15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Dividend: ");
