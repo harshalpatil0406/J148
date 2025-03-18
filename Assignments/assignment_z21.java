a9e0275b993b121a1fbfc98fb00b5db6615396c1
harshalpatil0406 <140956375+harshalpatil0406@users.noreply.github.com>
2025-03-18 09:04:57 +0530

    Update and rename assignment_21.java to assignment_z21.java

--- Assignments/assignment_z21.java
+++ Assignments/assignment_z21.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 //Reverse Array
-public class assignment_21 {
+public class assignment_z21 {
     public static void main(String[] args) {
         int[] arr = {12,45,23,87,67,55};
         int[] arr1 = Arrays.copyOf( arr,arr.length );
