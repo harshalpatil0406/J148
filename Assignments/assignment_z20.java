51c94f64f3e67cee0f230a5e51a66abd46053a43
harshalpatil0406 <140956375+harshalpatil0406@users.noreply.github.com>
2025-03-18 09:04:37 +0530

    Update and rename assignment_20.java to assignment_z20.java

--- Assignments/assignment_z20.java
+++ Assignments/assignment_z20.java
@@ -3,7 +3,7 @@ package Assignments;
 import java.util.Scanner;
 
 //Calculate Average of marks using array
-public class assignment_20 {
+public class assignment_z20 {
 
     public static void main(String[] args) {
         int[] number = {12,45,34,88,99};
